70987e6fbbfe3f930c13456082a3b0c015a5f765
abxijth <abhijithrpillai231@gmail.com>
2026-05-09 17:10:04 +0530

    fix: scrolling when clicked on menu buttons in mobile ui

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ let menuCloseButton = document.querySelector("#menu-close-button");
 menuCloseButton.addEventListener("click", () =>  {
         menuOpenButton.click();
 });
+
+let navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
+        document.body.classList.remove("show-mobile-menu");
+    })
+    
+});
